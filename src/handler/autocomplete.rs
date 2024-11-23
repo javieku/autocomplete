dc40b389ebf91a1223c3ba519382fa21be4a9c76
javieku <javieku@gmail.com>
2024-11-23 22:46:51 +0100

    Customize axum deserialization errors

--- src/handler/autocomplete.rs
+++ src/handler/autocomplete.rs
@@ -5,17 +5,18 @@ use crate::server::state::AppState;
 use crate::service::autocomplete;
 use anyhow::Result;
 use axum::extract::State;
+use axum::response::{IntoResponse, Response};
 use axum::routing::post;
-use axum::Json;
 use axum::Router;
+use axum_macros::FromRequest;
 use garde::Validate;
 use log::{info, trace};
 
 #[axum_macros::debug_handler]
 pub async fn autocomplete(
     State(state): State<AppState>,
-    Json(request): Json<AutocompleteRequest>,
-) -> Result<Json<AutocompleteResponse>, AutocompleteError> {
+    JsonWithCustomError(request): JsonWithCustomError<AutocompleteRequest>,
+) -> Result<JsonWithCustomError<AutocompleteResponse>, AutocompleteError> {
     if let Err(e) = request.validate() {
         info!("Invalid request: {e}");
         return Err(AutocompleteError::from(e));
@@ -24,7 +25,7 @@ pub async fn autocomplete(
     match autocomplete::get_suggestions(&state, request).await {
         Ok(response) => {
             trace!("Successfully found suggestions");
-            Ok(Json(response))
+            Ok(JsonWithCustomError(response))
         }
         Err(e) => {
             trace!("There was an error while fetching suggestions {e:?}");
@@ -41,4 +42,17 @@ pub fn create_router_app(state: AppState) -> Router {
     let router = Router::new();
     let router = self::add_routers(router);
     router.with_state(state)
+}
+
+#[derive(FromRequest)]
+#[from_request(via(axum::Json), rejection(AutocompleteError))]
+pub struct JsonWithCustomError<T>(T);
+
+impl<T> IntoResponse for JsonWithCustomError<T>
+where
+    axum::Json<T>: IntoResponse,
+{
+    fn into_response(self) -> Response {
+        axum::Json(self.0).into_response()
+    }
 }
